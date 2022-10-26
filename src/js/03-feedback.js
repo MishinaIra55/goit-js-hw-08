1483bba22e68326573546a6e9c442c52001ad193
MishynaIra <gortenzia1987@gmail.com>
2022-10-26 14:16:32 +0300

    module 8 task 3.1 обработала input и сохранение event

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,20 @@
+// Отслеживай на форме событие input, и каждый раз записывай в локальное хранилище объект с полями email и message,
+//  в которых сохраняй текущие значения полей формы. Пусть ключом для хранилища будет строка "feedback-form-state".
+const input = document.querySelector('.feedback-form');
+
+      input.addEventListener('input', (event) => {
+         const {
+            elements: {email, message}
+         } = event.currentTarget;
+
+         const settings = {
+            email : email.value,
+            message: message.value,
+         };
+         
+
+         localStorage.setItem("feedback-form-state", JSON.stringify(settings));
+});
+
+
 
