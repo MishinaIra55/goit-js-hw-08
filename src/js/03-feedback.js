fd476fa347ae206004eb088cfb79365942458eba
MishynaIra <gortenzia1987@gmail.com>
2022-10-26 17:28:47 +0300

    add task 3 module 8

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,34 +1,46 @@
+import throttle from "lodash.throttle";
+
 // Отслеживай на форме событие input, и каждый раз записывай в локальное хранилище объект с полями email и message,
 //  в которых сохраняй текущие значения полей формы. Пусть ключом для хранилища будет строка "feedback-form-state".
 const input = document.querySelector('.feedback-form');
 
-      input.addEventListener('input', (event) => {
-         const {
-            elements: {email, message}
-         } = event.currentTarget;
-
-         const settings = {
-            email : email.value,
-            message: message.value,
-         };
-
-         localStorage.setItem("feedback-form-state", JSON.stringify(settings));
+input.addEventListener('input', updateForm);  
          
-         const savedSettings = localStorage.getItem("feedback-form-state");
-         const parsedSettings = JSON.parse(savedSettings);
-         console.log(parsedSettings);
+function updateForm (event) {
+   console.log('sf', event.currentTarget);
+   const {
+      elements: {email, message}
+   } = event.currentTarget;
+
+   const settings = {
+      email : email.value,
+      message: message.value,
+   };
+
+   localStorage.setItem("feedback-form-state", JSON.stringify(settings));
+}
+
+
+
+window.addEventListener('load', () => {
+   const localSet = localStorage.getItem("feedback-form-state");
+   if (localSet) {
+      const parsedSettings = JSON.parse(localSet);
+         input.querySelector('[name = email]').value = parsedSettings.email;
+         input.querySelector('[name = message]').value = parsedSettings.message;
+   }
 });
 
-//При сабмите формы 
 input.addEventListener('submit', onSubmitForm);
 
 function onSubmitForm (event) {
    //отменяем перезагрузку страницы
    event.preventDefault();
-  // Очищаем поля формы 
+   console.log('data', JSON.parse(localStorage.getItem("feedback-form-state")));
+   // Очищаем поля формы
    event.target.reset(); 
    //Очищаем хоанилище
-    localStorage.removeItem('feedback-form-state');
+   localStorage.removeItem('feedback-form-state');
 }
 
 
