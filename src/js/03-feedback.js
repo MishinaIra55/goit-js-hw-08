b5756afd803a04c948346227a6931ac3b581ba5f
MishynaIra <gortenzia1987@gmail.com>
2022-10-27 11:18:38 +0300

    fixed throttle

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,29 +1,26 @@
 import throttle from "lodash.throttle";
 
+const STORAGE_KEY = "feedback-form-state";
 // Отслеживай на форме событие input, и каждый раз записывай в локальное хранилище объект с полями email и message,
 //  в которых сохраняй текущие значения полей формы. Пусть ключом для хранилища будет строка "feedback-form-state".
 const input = document.querySelector('.feedback-form');
 
-input.addEventListener('input', updateForm);  
+input.addEventListener('input', throttle(updateForm, 500));  
          
 function updateForm (event) {
-   console.log('sf', event.currentTarget);
-   const {
-      elements: {email, message}
-   } = event.currentTarget;
 
    const settings = {
-      email : email.value,
-      message: message.value,
+      email : event.target.value,
+      message: event.target.value,
    };
 
-   localStorage.setItem("feedback-form-state", JSON.stringify(settings));
+   localStorage.setItem(STORAGE_KEY, JSON.stringify(settings));
 }
 
 
 
 window.addEventListener('load', () => {
-   const localSet = localStorage.getItem("feedback-form-state");
+   const localSet = localStorage.getItem(STORAGE_KEY);
    if (localSet) {
       const parsedSettings = JSON.parse(localSet);
          input.querySelector('[name = email]').value = parsedSettings.email;
@@ -36,11 +33,11 @@ input.addEventListener('submit', onSubmitForm);
 function onSubmitForm (event) {
    //отменяем перезагрузку страницы
    event.preventDefault();
-   console.log('data', JSON.parse(localStorage.getItem("feedback-form-state")));
+   console.log('data', JSON.parse(localStorage.getItem(STORAGE_KEY)));
    // Очищаем поля формы
    event.target.reset(); 
    //Очищаем хоанилище
-   localStorage.removeItem('feedback-form-state');
+   localStorage.removeItem(STORAGE_KEY);
 }
 
 
